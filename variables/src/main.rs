73bbbf4397b70bfdb6c137d1ec4e4b1415c71e39
expilu <dani.lopez.lacalle@proton.me>
2025-12-16 18:01:37 +0000

    ✨ feat(variables): add examples for variable mutability and arithmetic operations

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,24 @@
 fn main() {
-    let x = 5;
-
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
+    let mut x = 5;
     println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    let a = [1, 2, 3, 4, 5];
 }
