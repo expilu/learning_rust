6e425bcbd6bdd99110d24de3ce1417568310ef9f
expilu <dani.lopez.lacalle@proton.me>
2025-12-21 22:07:55 +0000

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,33 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = build_user(String::from("foo@bar.com"), String::from("Foo"));
+
+    let user3 = User {
+        email: String::from("bar@baz.com"),
+        ..user2
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
