33952abb0ddaf7768cf7d4dc4b1ce92e21c4ab66
expilu <dani.lopez.lacalle@proton.me>
2025-12-22 18:26:14 +0000

    enums

---
+++ enums/src/main.rs
@@ -0,0 +1,30 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr2 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(four);
+    route(six);
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let home = IpAddr2::V4(127, 0, 0, 1);
+    let loopback = IpAddr2::V6(String::from("::1"));
+}
