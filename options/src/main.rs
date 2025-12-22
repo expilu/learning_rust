4169633f1d9788d539984ccd03eab252ec45bd15
expilu <dani.lopez.lacalle@proton.me>
2025-12-22 19:59:22 +0000

    ✨ feat(options): initialize new Rust project with Option enum examples

---
+++ options/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let some_number = Some(5);
+    let some_char = Some('a');
+
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    // let sum = x + y; // won't compile
+}
